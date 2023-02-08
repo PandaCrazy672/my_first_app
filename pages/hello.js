a6d32cc12934c714998255ff7d4371ed3acb53a9
unknown <elatiguib@gmail.com>
2023-02-08 16:41:08 +0100

    initial commit

---
+++ pages/hello.js
@@ -0,0 +1,10 @@
+const Hello = () => {
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <p>This is DEFINITELY my first Next App, pls help</p>  
+        </>
+    )
+}
+
+export default Hello;
